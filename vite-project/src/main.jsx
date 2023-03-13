69bcb2843b8d6cff565f1cfe98da2c2d51902aab
mateonlienard <mateonlienard@gmail.com>
2023-03-13 19:10:51 -0300

    sume una carpeta con las imagenes de mis productos, y modifica el main dado q sume react-router

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hola</div>,
+  },
+  {
+    path: '/category/:id',
+    element: <div>hola</div>,
+  },
+  {
+    path: '/item/:id',
+    element: <div>hola</div>,
+  },
+  {
+    path: '/cart',
+    element: <div>hola</div>,
+  },
+  {
+    path: '/checkout',
+    element: <div>hola</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
